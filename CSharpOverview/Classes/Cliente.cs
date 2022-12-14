d9919ddc54d74c38ae4686451a2c937755bb53c0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-12-14 09:24:24 -0300

    Sintaxe de Classes

---
+++ CSharpOverview/Classes/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOverview.Classes
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+
+        public Cliente(string nome, string cpf, string profissao)
+        {
+            this.Nome = nome;
+            this.Cpf = cpf;
+            this.Profissao = profissao;  
+        }
+    }
+}
---
+++ CSharpOverview/Classes/ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOverview.Classes
+{
+    public class ContaCorrente
+    {
+
+        private int numeroAgencia;
+        private double saldo;
+        public Cliente Cliente { get; }
+
+        public string Conta { get; set; }
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public int NumeroAgencia { 
+            get { return numeroAgencia;} 
+            private set {
+                if (value > 0)
+                    this.numeroAgencia = value;
+            } 
+        }
+
+        public ContaCorrente(int numeroAgencia, string numeroConta, Cliente titular)
+        {
+            this.numeroAgencia = numeroAgencia;
+            this.Conta = numeroConta;
+            this.Cliente = titular;
+            TotalDeContasCriadas++;
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+
+        public void SetSaldo(double valor)
+        {
+            
+            this.saldo = valor;
+        }
+
+
+        public void Despositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo > valor)
+            {
+                saldo -= valor;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Tranferir(double valor, ContaCorrente contaDestino)
+        {
+            if (contaDestino.saldo < valor)
+                return false;
+
+            Sacar(valor);
+            contaDestino.Despositar(valor);
+
+            return true;
+        }
+
+    }
+}
